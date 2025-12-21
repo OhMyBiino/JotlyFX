4ae6776b4058d101c56cf78af2da25dd7017a7d2
ohmybiino <davenarzobal@gmail.com>
2025-12-21 17:49:45 +0800

    Setup Initial Window Size and Jotly's Logo

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,40 +1,52 @@
 package application;
 	
+
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
     
  public static void main(String[] args) {
         launch(args);
     }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Group root = new Group();
+		Scene scene = new Scene(root,750,500,Color.WHITE);
+		
+		//Set Jotly's Title
+		stage.setTitle("Jotly");
+		stage.setResizable(false);
+		
+		//Set Jotly's Logo
+		Image jotlyImage = new Image("Jotly_Logo.png");
+		stage.getIcons().add(jotlyImage);
+		
+		//Sample Text
+		Text text = new Text();
+		text.setText("Hello Buddy");
+		text.setX(450/2 - 5);
+		text.setY(450/2 - 3);
+		
+		
+		root.getChildren().add(text);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
